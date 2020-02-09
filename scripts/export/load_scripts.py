9774e45fb28bf7cdd2a3ff41ed3d371a83da4680
rgerum <richard.gerum@yahoo.de>
2020-02-09 12:29:42 +0100

    added load script to update exporter in a .blend file

---
+++ scripts/export/load_scripts.py
@@ -0,0 +1,33 @@
+import bpy
+
+filenames = [
+    "config.py",
+    "material.py",
+    "mesh.py",
+    "report.py",
+    "skeleton.py",
+    "ui.py",
+    "util.py",
+    "xml_simple.py",
+    "load_scripts.py",
+]
+
+def getPath(file):
+    if file == "ui.py":
+        return "//scripts/"+file
+    else:
+        return "//scripts/export/"+file    
+
+# iterate over all filenames
+for file in filenames:
+    # remove the file if it exits
+    if file in bpy.data.texts:
+        bpy.data.texts.remove(bpy.data.texts[file])
+    # and open it from the provided path
+    bpy.ops.text.open(filepath=getPath(file))
+
+# register the ui.py text as a module (it gets called on load of the .blend file)
+bpy.data.texts["ui.py"].use_module = True
+bpy.context.space_data.text = bpy.data.texts["ui.py"]
+bpy.ops.text.run_script()
+bpy.context.space_data.text = bpy.data.texts["load_scripts.py"]
